1cdfee98d629f87e6c5371ef1527902753a6e9f1
Mark Anthony Alvarez <markelotone@gmail.com>
2021-04-09 12:28:59 +1000

    Fixed the button issue for mobile responsiveness and added title

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {isMobile} from 'react-device-detect';
 
 // import ReactDOM from 'react-dom';
 
@@ -9,40 +10,43 @@ export default class Header extends Component {
             display: 'inline-block',
             hidden: true,
             hide: 'none',
+            isMobile: '',
         }
         this.ActionLink = this.ActionLink.bind(this);
     }
 
     ActionLink() {
-        function handleClick(e) {
-          e.preventDefault();
-          console.log('The link was clicked.');
-        }
+        // function handleClick(e) {
+        //   e.preventDefault();
+        //   console.log('The link was clicked.');
+        // }
         if (this.state.hidden === true) {
             this.setState({
                 hidden: false,
+                hide: 'inline-block'
             });
-        console.log("Now Hiding Menu")
+        console.log("Now Showing Menu")
         }
         if (this.state.hidden === false) {
             this.setState({
                 hidden: true,
+                hide: 'none',
             });
-            console.log("Now Showing Menu")
+            console.log("Now Hiding Menu")
             }
       }
 
     render () {
-        const isHidden = this.state.hidden;
+        // const isHidden = this.state.hidden;
         let menuShow;
-        if (isHidden) {
-            menuShow = <nav className="menu" style={{display: this.state.display}}>
+        if (isMobile) {
+            menuShow = <nav className="menu" style={{display: this.state.hide}}>
             <a href="#"> Haircuts </a>
             <a href="#"> About Us </a>
             <a href="#"> Contact </a>
         </nav>;
         } else {
-            menuShow = <nav className="menu" style={{display: this.state.hide}}>
+            menuShow = <nav className="menu" style={{display: this.state.display}}>
             <a href="#"> Haircuts </a>
             <a href="#"> About Us </a>
             <a href="#"> Contact </a>
--- src/Components/MainJumbo.js
+++ src/Components/MainJumbo.js
@@ -14,7 +14,8 @@ export default class MainJumbo extends Component {
             <section id="MainJumbo">
                 <div className="jumboContainer">
                     <div className="imageBackground"> </div>
-                    <div className="jumboTitle"> Johnny's Barber </div>
+                    <div className="jumboTitle"> Johnny's Barber Shop</div>
+                    <div className="jumboDescription">Specialising in modern and traditional haircuts</div>
                 </div>
             </section>
           );
